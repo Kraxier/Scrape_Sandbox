f4d0b210008bc70b612bf463dd0c14bae7a745d3
Kraxiers <binaskristian@gmail.com>
2025-03-30 04:20:37 +0800

    Starting to Scrape the Sandbox of Scrapping

---
+++ test.py
@@ -0,0 +1,12 @@
+# https://toscrape.com/
+# Scrapping Everything on This Website
+'''
+Bookstoscrape is Done 
+
+Next is Quote_To_Scrape
+    0. Micro Data with Pagination
+    1. Log in With CRSF Token
+    2. Javascript Generated Content
+    3. 
+'''
+
